b04f71fb83224d120a649a61c22dc4ed21062420
brytsk <thienminh.npn@gmail.com>
2026-04-04 13:45:04 +1000

    added ccbh for colcon build in the current folder

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ sudo cp myros.sh ~/
 # sudo cp rules/*  /etc/udev/rules.d/
 sudo cat ./.bashrc >> ~/.bashrc
 # sudo cp gitme.sh /usr/bin/
-sudo cp ccb.sh /usr/bin/ccb
+sudo cp ccb.sh /usr/bin/ccb; sudo chmod +x /usr/bin/ccb;
+sudo cp ccbh.sh /usr/bin/ccbh; sudo chmod +x /usr/bin/ccbh;
 # sudo chmod +x /usr/bin/gitme.sh
 echo "done."
